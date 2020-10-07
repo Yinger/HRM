4e4a292d1a02cce58d87ec8007c8bddbdb89a73c
林 <melody.crazycoding@gmail.com>
2020-10-07 18:13:43 +0800

    server create & update

--- server-express/routes/employee.ts
+++ server-express/routes/employee.ts
@@ -1,12 +1,15 @@
 import express from "express";
+import bodyParser from "body-parser";
 import Department from "../models/Department";
 import Level from "../models/Level";
 import Employee from "../models/Employee";
 import { resolve } from "bluebird";
-import { QueryTypes } from "sequelize";
+import { QueryTypes, UpdateOptions } from "sequelize";
 import dbConfig from "../config/db";
 
 const router = express.Router();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 let queryAllSQL = `SELECT employee.*, level.level, department.department
     FROM employee, level, department
     WHERE
@@ -78,6 +81,50 @@ router.get("/getEmployee", async function (req, res) {
       // total: total,
     })
   );
+});
+
+router.post("/createEmployee", urlencodedParser, async function (req, res) {
+  let { name, departmentId, hiredate, levelId } = req.body;
+  Employee.create({
+    name: name,
+    hiredate: new Date(hiredate),
+    departmentId: departmentId,
+    levelId: levelId,
+  }).then((result) => {
+    resolve(
+      res.json({
+        flag: 0,
+        data: {
+          key: result.id,
+          id: result.id,
+        },
+        msg: "OK",
+      })
+    );
+  });
+});
+router.post("/updateEmployee", urlencodedParser, async function (req, res) {
+  let { id, name, departmentId, hiredate, levelId } = req.body;
+  const options: UpdateOptions = {
+    where: { id: id },
+    limit: 1,
+  };
+  Employee.update(
+    {
+      name: name,
+      departmentId: departmentId,
+      hiredate: new Date(hiredate),
+      levelId: levelId,
+    },
+    options
+  ).then(() => {
+    resolve(
+      res.json({
+        flag: 0,
+        msg: "OK",
+      })
+    );
+  });
 });
 
 export default router;
